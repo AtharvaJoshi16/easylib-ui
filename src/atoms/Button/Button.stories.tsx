427243d73564618f78bd9b1d66d599ff61bd6be7
AtharvaJoshi16 <aj962788@gmail.com>
2025-05-27 13:51:06 +0530

    Fixed build

---
+++ src/atoms/Button/Button.stories.tsx
@@ -0,0 +1,23 @@
+import type { StoryObj } from "@storybook/react";
+
+import Button from "./Button";
+
+const meta = {
+  title: "Components/Atoms/Button",
+  component: Button,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+  args: {
+    children: "Button",
+  },
+};
